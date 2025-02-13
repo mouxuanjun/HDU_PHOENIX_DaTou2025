972d39d526329672e9ebd628d9c92b043789eb92
HWX <1583164119@qq.com>
2025-02-14 06:04:34 +0800

    无

--- Control/Computer.h
+++ Control/Computer.h
@@ -28,7 +28,7 @@ char type;                      //1 消息类型 0xB0
 float yaw;                      //2-5 偏航角
 float pitch;                    //6-9 俯仰角
 uint8_t enemy_team_color;       //10 敌方颜色 0红，1蓝
-uint8_t mode;                   //11 模式 0自瞄，1符
+uint8_t mode;                   //11 模式 0自瞄 1打符
 uint8_t rune_flag;              //12 符模式 0不可激活，1小符，2大符
                                 //......  13-30 预留空位
 char end;                       //31 帧尾 'e’
